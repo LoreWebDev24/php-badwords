e56b01e1d7faecda88a22576e3f31663e5f27df9
LoreWebDev24 <lorenzo.delre12@gmail.com>
2023-11-21 15:23:53 +0100

    add full exercise: words replacer

--- form.php
+++ form.php
@@ -1,7 +1,10 @@
 <?php
 $badWord = $_POST['bad-word'];
-
+$text = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero quis est explicabo minima itaque? Doloremque reprehenderit dolores officia reiciendis rem mollitia adipisci, eum hic nulla harum quas quis libero distinctio molestias esse dolorum perferendis, atque consequatur optio sint aliquid voluptatum incidunt illum pariatur. Quasi labore optio maxime. Iste, quo dolore. Harum ratione qui, dolorem asperiores, reprehenderit doloremque autem voluptas, libero provident optio iure repellendus deserunt dicta ea omnis nobis adipisci quam. Architecto harum ad deleniti eum accusantium! Eius doloremque saepe accusantium recusandae sapiente necessitatibus possimus facilis molestias explicabo deserunt repudiandae mollitia doloribus minima delectus tenetur, fugiat soluta est aut. Voluptates magnam omnis praesentium totam, voluptatem repellendus optio molestiae libero? Quas voluptatum, nihil voluptas iste consectetur aliquid amet ex eligendi ullam.';
+$replaceWith = '****';
+$textCensored = str_replace($badWord, $replaceWith, $text);
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -14,10 +17,21 @@ $badWord = $_POST['bad-word'];
 
 <body>
 
+<style>
+    .text-uncensored {
+        display: none;
+    }
+</style>
+
   <div>
-    <p>
+    <p class="text-uncensored"><?php echo $text; ?></p>
+    <h1>
         Il tuo testo censurato
+    </h1>
+    <p>
+        <?php echo $textCensored; ?>
     </p>
+    <h4>Il nuovo paragrafo è lungo: <?php echo strlen($textCensored); ?> caratteri</h4>
   </div>
 
 </body>
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
-
+$text = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero quis est explicabo minima itaque? Doloremque reprehenderit dolores officia reiciendis rem mollitia adipisci, eum hic nulla harum quas quis libero distinctio molestias esse dolorum perferendis, atque consequatur optio sint aliquid voluptatum incidunt illum pariatur. Quasi labore optio maxime. Iste, quo dolore. Harum ratione qui, dolorem asperiores, reprehenderit doloremque autem voluptas, libero provident optio iure repellendus deserunt dicta ea omnis nobis adipisci quam. Architecto harum ad deleniti eum accusantium! Eius doloremque saepe accusantium recusandae sapiente necessitatibus possimus facilis molestias explicabo deserunt repudiandae mollitia doloribus minima delectus tenetur, fugiat soluta est aut. Voluptates magnam omnis praesentium totam, voluptatem repellendus optio molestiae libero? Quas voluptatum, nihil voluptas iste consectetur aliquid amet ex eligendi ullam.'
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -17,7 +18,8 @@
     <input type="submit" value="Submit">
   </form>
     <h2>TEXT UNCENSORED:</h2>
-    <p class="text-uncensored">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero quis est explicabo minima itaque? Doloremque reprehenderit dolores officia reiciendis rem mollitia adipisci, eum hic nulla harum quas quis libero distinctio molestias esse dolorum perferendis, atque consequatur optio sint aliquid voluptatum incidunt illum pariatur. Quasi labore optio maxime. Iste, quo dolore. Harum ratione qui, dolorem asperiores, reprehenderit doloremque autem voluptas, libero provident optio iure repellendus deserunt dicta ea omnis nobis adipisci quam. Architecto harum ad deleniti eum accusantium! Eius doloremque saepe accusantium recusandae sapiente necessitatibus possimus facilis molestias explicabo deserunt repudiandae mollitia doloribus minima delectus tenetur, fugiat soluta est aut. Voluptates magnam omnis praesentium totam, voluptatem repellendus optio molestiae libero? Quas voluptatum, nihil voluptas iste consectetur aliquid amet ex eligendi ullam.</p>
+    <p class="text-uncensored"><?php echo $text; ?></p>
+    <h4>Il paragrafo è lungo: <?php echo strlen($text); ?> caratteri</h4>
 </body>
 
 </html>
